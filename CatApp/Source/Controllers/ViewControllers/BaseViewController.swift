434a2972159bb7511d6640e1eba0cb3adb060d7c
guidofa <guidof@nimble.la>
2020-09-26 17:32:36 -0300

    Syling

--- CatApp/Source/Controllers/ViewControllers/BaseViewController.swift
+++ CatApp/Source/Controllers/ViewControllers/BaseViewController.swift
@@ -10,6 +10,12 @@ import SideMenu
 import UIKit
 
 class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.tintColor = .black
+    }
+    
     @IBAction private func hamburgerButtonOnClick() {
         showSideMenu()
     }
