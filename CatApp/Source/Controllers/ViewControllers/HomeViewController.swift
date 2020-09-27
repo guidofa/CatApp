2431c537e23f64b00298cb62b53414f68c03f368
guidofa <guidof@nimble.la>
2020-09-26 22:57:08 -0300

    ADD: Favourite implementation

--- CatApp/Source/Controllers/ViewControllers/HomeViewController.swift
+++ CatApp/Source/Controllers/ViewControllers/HomeViewController.swift
@@ -13,6 +13,10 @@ class HomeViewController: BaseViewController {
     @IBAction private func buttonOnClick() {
         setACatImage()
     }
+    @IBAction private func addFavourite() {
+        addTheImageToFavourite()
+    }
+    fileprivate var imageId: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +27,19 @@ class HomeViewController: BaseViewController {
         // What happen if the URL is nil ?
         view.showLoader()
         HomeService.getRandomCat({ (randomCat) in
+            self.imageId = randomCat.id
             self.catImageView.load(url: URL(string: randomCat.url!)!, view: self.view)
         }, errorHandler: { (error) -> Void in
             self.view.hideLoader()
+            print(error?.localizedDescription ?? "")
+        })
+    }
+    
+    func addTheImageToFavourite() {
+        guard let id = imageId else { return }
+        FavouriteService.addFavourite(id, subId: "jhondoe123", callback: { (message) in
+            self.showAlert(title: "Favourite", message: message, alertActionTitle: "accept")
+        }, errorHandler: { (error) -> Void in
             print(error?.localizedDescription ?? "")
         })
     }
---
+++ CatApp/Source/Networking/Services/FavouriteService.swift
@@ -0,0 +1,37 @@
+//
+//  FavouriteService.swift
+//  CatApp
+//
+//  Created by Macbook Pro Nimble on 9/26/20.
+//  Copyright © 2020 Guido Fabio. All rights reserved.
+//
+
+import SwiftyJSON
+
+class FavouriteService: BaseWebService {
+    class func addFavourite(_ imageId: String, subId: String, callback: @escaping (_ message: String) -> Void, errorHandler: @escaping ErrorHandler) {
+        
+        let url = BaseWebService.getBaseURL() + "favourites"
+        
+        let params = ["image_id": imageId, "sub_id": subId]
+        
+        NetworkManager.sharedInstance.POST(url, params: params, completionHandler: processResponse({ (data) -> Void in
+            
+            if data != nil {
+                
+                let response = JSON(data!)
+                
+                if let response = response["message"].string {
+                    return callback(response)
+                } else {
+                    return callback("Error")
+                }
+            }
+            
+            errorHandler(NSError.initWithMessage("Error adding the image in favourites!"))
+            
+            }, errorHandler: errorHandler))
+    }
+}
+
+
